4e1bb564bbeec8911684d1ec0b279af1db93275e
Anugerah Samar Gandhi <anugerahsamargandhi@Anugerahs-MacBook-Air.local>
2026-05-05 00:54:20 +0800

    Tambah login, role admin & user, autentikasi dan otorisasi

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>Dashboard</h2>
+    </x-slot>
+
+    <div>
+        Selamat datang di dashboard!
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
-use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/mahasiswa/create', [MahasiswaController::class, 'create']);
+Route::get('/', function () {
+    return  redirect('/login');
+});
 
-Route::post('/mahasiswa/store', [MahasiswaController::class, 'store']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
